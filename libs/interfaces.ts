27db6e0b2150013cdcf75545091e3251226a3f51
nsretkovicc <nikolasretkovic@webelinx.com>
2023-09-13 00:10:16 +0200

    Define interfaces

---
+++ libs/interfaces.ts
@@ -0,0 +1,51 @@
+export interface AuthState  {
+    isAuth:Boolean,
+    username:string,
+    password:string,
+    uid: string,
+}
+
+export interface LoginPayload {
+    username:string,
+    password:string
+}
+
+export interface User {
+    _id?:string,
+    fullName:string,
+    username:string,
+    password:string,
+    description:string,
+    isCompany:boolean,
+    rating:number,
+    // For company
+    postedJobs: Job[],
+    // For user
+    jobsApplied: Job[],
+}
+
+export interface Job {
+    _id?:string,
+    title:string,
+    seniority:string,
+    // Here, company id is provided, when user clicks on company's name on a job, it redirects to /companies/:id 
+    companyId:string,
+    tag: Tag,
+    description:string,
+    lookingForDescription:string,
+    companyOffersDescription:string,
+    location:string,
+    candidates: Candidate[]
+}
+
+// Short information about a user that will be displayed when user applies as a candidate for a job
+export interface Candidate {
+    userId:string,
+    username:string,
+    rating:number
+}
+
+export interface Tag {
+    name:string,
+    jobs: string[]
+}
